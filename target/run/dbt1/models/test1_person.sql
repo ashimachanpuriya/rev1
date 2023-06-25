66c935cf45b6a9f051ef1ca73b95ad8320b09ea7
ashimachanpuriya <chanpuriyaashima@gmail.com>
2023-06-26 00:30:02 +0530

    first

---
+++ target/run/dbt1/models/test1_person.sql
@@ -0,0 +1,7 @@
+
+  create or replace   view DBT.TEST.test1_person
+  
+   as (
+    select * from persons
+  );
+
---
+++ target/run/dbt1/models/test2_employee.sql
@@ -0,0 +1,7 @@
+
+  create or replace   view DBT.TEST.test2_employee
+  
+   as (
+    select * from employee
+  );
+
